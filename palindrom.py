8cdb7af583ade339e4b882b94add759db872c21c
KRAMOLNEAT CHITSUE <ma_meaw2009@yahoo.com>
2020-10-04 16:04:59 +0700

    palindorm

--- palindrom.py
+++ palindrom.py
@@ -1,9 +1,9 @@
-text = 'mofvbnm,m'
-for i in range (0,len(text)):
-    if i < len(text)/2:
-        if text[i] == text[len(text)-i-1]:
-            print('palindorm')
-        else:
-            print('not')
-        
-        
+import sys
+text = sys.argv[1]
+ispalindrom = True
+for i in range (0,len(text)/2):
+    if text[i] != text[len(text)-i-1]:
+        ispalindrom = False
+        break
+
+print(text+' is'+('' if ispalindrom else ' not')+' palindrom')
